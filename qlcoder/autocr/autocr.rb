d4f5dc4c3b383cce51add61320b6256aa8763d7e
Jingkai Tang <jingkaitang@gmail.com>
2016-09-14 17:25:55 +0800

    mix: autocr

--- qlcoder/autocr/autocr.rb
+++ qlcoder/autocr/autocr.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'parallel'
+
 START = 's'
 EMPTY = '0'
 BLOCK = '1'
@@ -6,9 +8,11 @@ UP = '^'
 DOWN = 'v'
 LEFT = '<'
 RIGHT = '>'
+DYE = 'd'
 
 class AutoCR
     attr_accessor :data, :l, :c, :start, :cursor, :path
+
     def initialize(data='', l=0, c=0, start=0)
         @data = data
         @l = l
@@ -114,7 +118,7 @@ class AutoCR
     def show
         for i in 0...@l
             puts('- '*(@c*2+1))
-            puts('| ' + @data.slice(i*@c, @c).split('').join(' | ') + ' |')
+            puts('| ' + (@data.slice(i*@c, @c).split('').map{|x| x == '0' ? "\e[35m#{x}\e]" : "\e[33m#{x}\e]"}).join(' | ') + ' |')
         end
         puts('- '*(@c*2+1))
     end
@@ -123,40 +127,54 @@ class AutoCR
         x, y = cov1to2(@start)
         return "#{x+1} #{y+1} #@path"
     end
+
+    def solvable?
+        cr = clone
+        s = @data.index(EMPTY)
+        return true unless s
+        self.class.dye(cr, s)
+        return cr.is_covered?
+    end
+
+    def self.dye(cr, i)
+        return if cr.data[i] != EMPTY
+        cr.data[i] = DYE
+        x, y = cr.cov1to2(i)
+        dye(cr, cr.cov2to1(x-1, y)) if x > 0
+        dye(cr, cr.cov2to1(x+1, y)) if x < cr.l-1
+        dye(cr, cr.cov2to1(x, y-1)) if y > 0
+        dye(cr, cr.cov2to1(x, y+1)) if y < cr.c-1
+    end
 end
 
 def solve(data, l, c)
-    start = data.index(EMPTY)
+    starts = (0...data.length).select {|i| data[i] == EMPTY}
+    starts.shuffle!
     up = -> cr {cr.up}
     right = -> cr {cr.right}
     down = -> cr {cr.down}
     left = -> cr {cr.left}
-    action = [up, right, down, left]
-    while start < data.length
+    actions = [up, right, down, left]
+    Parallel.map(starts, in_processes: 4) do |start|
         crs = [AutoCR.new(data.clone, l, c, start)]
         until crs.empty?
             cr = crs.pop
-            fwd = cr.forward
-            for i in 0...4
-                if fwd[i]
-                    ccr = cr.clone
-                    action[i].call(ccr)
-                    if ccr.is_covered?
-                        return ccr.result
-                    else
-                        crs.push(ccr)
-                    end
+            fwd = cr.forward.zip(actions).select {|f, a| f}
+            if fwd.empty? && cr.is_covered?
+                puts cr.result
+                raise Parallel::Kill
+            end
+            fwd.each_with_index do |(_, action), i|
+                ccr = (i+1 == fwd.length) ? cr : cr.clone
+                action.call(ccr)
+                if ccr.solvable?
+                    crs.push(ccr)
                 end
             end
-        end
-        start += 1
-        while start < data.length && data[start] != EMPTY
-            start += 1
         end
     end
 end
 
 x, y, map = ARGV
-ret = solve(map.dup, x.to_i, y.to_i)
-puts ret
+solve(map.dup, x.to_i, y.to_i)
 
--- qlcoder/autocr/solve.sh
+++ qlcoder/autocr/solve.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+date "+%Y-%m-%d %H:%M:%S"
 question=http://www.qlcoder.com/train/autocr
 cookie=`cat cookiefile`
-page=`curl -b "$cookie"  "$question"`
+page=`curl -b "$cookie"  "$question" 2> /dev/null`
 level=`echo $page | grep -Po 'level=([0-9]+)&x=([0-9]+)&y=([0-9]+)&map=([0-1]+)'`
 echo $level
 condition=`echo "$level" | awk -F '=|&' '{print($4 " " $6 " " $8)}'`
 answer=`./autocr.rb $condition`
 echo $answer
 qcode=`echo "$answer" | awk '{print("x=" $1 "&y=" $2 "&path=" $3)}'`
-echo $qcode
 submit=http://www.qlcoder.com/train/crcheck?$qcode
 curl -b "$cookie" "$submit"
+
 
